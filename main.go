ee5de24e6c7cc16c1618aac604369870b52a4421
aidongsheng <18606563661@163.com>
2018-12-05 18:32:54 +0800

    爬取上海证券交易所数据

--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"github.com/PuerkitoBio/goquery"
+	"github.com/aidongsheng/go-scrapy/sse"
 	"log"
 )
 
-func ExampleScrape() {
-	doc, err := goquery.NewDocument("http://www.qiushibaike.com")
+//import (
+//	"fmt"
+//	"github.com/PuerkitoBio/goquery"
+//	"log"
+//)
+//
+//func ExampleScrape() {
+//	doc, err := goquery.NewDocument("http://www.qiushibaike.com")
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	doc.Find(".article").Each(func(i int, s *goquery.Selection) {
+//		if s.Find(".thumb").Nodes == nil && s.Find(".video_holder").Nodes == nil {
+//			content := s.Find(".content").Text()
+//			fmt.Printf("%s", content)
+//
+//		}
+//	})
+//}
+//
+//func main() {
+//	ExampleScrape()
+//}
+
+func main() {
+
+	var err error
+	err = sse.GetStockListA("/Users/josan/Documents/sseA.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
-	doc.Find(".article").Each(func(i int, s *goquery.Selection) {
-		if s.Find(".thumb").Nodes == nil && s.Find(".video_holder").Nodes == nil {
-			content := s.Find(".content").Text()
-			fmt.Printf("%s", content)
-		}
-	})
-}
+	err = sse.GetStockListB("/Users/josan/Documents/sseB.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-func main() {
-	ExampleScrape()
 }
